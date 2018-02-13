65d2c58bfba6716ea1bd291ea7e14619a1265aa9
Murat Bas <alonebasketballer@gmail.com>
2018-02-13 16:28:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 sum_error = 0
+iterations= 10000000
 data = [[1,3],
         [2,5],
         [3,7],
@@ -27,16 +28,20 @@ def hypothesis(a):
     return (Q0+Q1*a)
 
 
-for i in range (len(data)):
-    print(i,". DEGERLERİ",Q0,"------------",Q1)
-    squared_error = ((hypothesis(data[i][0])-data[i][1])**2)
-    print(i,". Squared Error Degeri",squared_error)
+for j in range (iterations):
+    ri = np.random.randint(len(data))
+    point = data[ri]
+
+    print(j,". DEGERLERİ",Q0,"------------",Q1)
+    squared_error = ((hypothesis(point[0])-point[1])**2)
+    print(j,". Squared Error Degeri",squared_error)
     sum_error += squared_error/6
-    tempQ0 = Q0 - learning_rate* ((hypothesis(data[i][0])-data[i][1])/3)
-    tempQ1 = Q1 - learning_rate*((((hypothesis(data[i][0])-data[i][1]))*data[i][0])/3)
+    tempQ0 = Q0 - learning_rate* ((hypothesis(point[0])-point[1])/3)
+    tempQ1 = Q1 - learning_rate*((((hypothesis(point[0])-point[1]))*point[0])/3)
     Q1 = tempQ1
     Q0 = tempQ0
-
+    if (squared_error==0):
+            break
 print("Toplam Error",sum_error)
 
 print("7 için tahmin",hypothesis(7))
